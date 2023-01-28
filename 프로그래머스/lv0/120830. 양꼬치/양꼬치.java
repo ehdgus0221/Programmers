4f6d80e46fc530a9407a8c0c71b8891969da164e
ehdgus0221 <66156319+ehdgus0221@users.noreply.github.com>
2023-01-28 16:43:50 +0900

    [level 0] Title: 양꼬치, Time: 0.02 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120830. 양꼬치/양꼬치.java
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.java
@@ -2,7 +2,7 @@ class Solution {
     public int solution(int n, int k) {
         int answer = 0;
         
-        int discount = n / 10;
+        int discount = n / 10;  // 음료수 서비스 개수
         
         answer = (12000 * n) +(2000 * (k - discount));
         
