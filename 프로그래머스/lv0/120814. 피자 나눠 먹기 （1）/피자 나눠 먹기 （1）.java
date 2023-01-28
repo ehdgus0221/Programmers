0a24ea37efa68756718284efaeff6c396ba12cd3
ehdgus0221 <66156319+ehdgus0221@users.noreply.github.com>
2023-01-28 17:45:30 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.01 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
+++ 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
@@ -1,17 +1,7 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        int k = 0;
-        
-        for(int i = 1; i <= n; i++) {
-            if(((7*k) + 1) <= n && n <= (7*(k+1))) {
-                answer = k+1;
-            }
-            else{
-                k += 1;
-            }
-        }
-        
+        int answer;
+        answer = (n + 6) / 7;
         return answer;
     }
 }
