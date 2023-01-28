15f728e11ff5ad7ebf5c4786e492bd2e196f950d
ehdgus0221 <66156319+ehdgus0221@users.noreply.github.com>
2023-01-28 16:34:56 +0900

    [level 0] Title: 배열의 평균값, Time: 0.04 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.java
+++ 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.java
@@ -11,3 +11,15 @@ class Solution {
         return answer;
     }
 }
+
+// 다른 사람 풀이
+
+// import java.util.Arrays;
+
+// class Solution {
+//     public double solution(int[] numbers) {
+//         return Arrays.stream(numbers).average().orElse(0);
+//      21번쨰 줄 해석 : Array.stream(배열 이름).averge() -> 합을 구하는 문제라면 sum()
+//     orElse(0); -> null 값이면 0을 반환
+//     }
+// }
