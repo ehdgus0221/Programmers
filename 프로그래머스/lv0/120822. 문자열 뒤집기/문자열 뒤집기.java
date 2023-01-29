6cf40af3a6131cf34eb5059ef019b1fa42192c67
ehdgus0221 <66156319+ehdgus0221@users.noreply.github.com>
2023-01-29 13:39:18 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.02 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,11 +1,19 @@
+// class Solution {
+//     public String solution(String my_string) {
+//         String answer = "";
+        
+//         return new answer(my_string).reverse().toString();
+        
+//         for(int i = 0; i < my_string.length(); i++){
+//             answer = answer + my_string.charAt(my_string.length()-1-i);
+//         }
+        
+//         return answer;
+//    }
+// }
+
 class Solution {
-    public String solution(String my_string) {
-        String answer = "";
-        
-        for(int i = 0; i < my_string.length(); i++){
-            answer = answer + my_string.charAt(my_string.length()-1-i);
-        }
-        
-        return answer;
+    public String solution(String myString) {
+        return new StringBuilder(myString).reverse().toString();
     }
 }
