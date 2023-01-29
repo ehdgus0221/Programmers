0a21754405e345b9955e7cc19ec31fb5287db825
ehdgus0221 <66156319+ehdgus0221@users.noreply.github.com>
2023-01-29 14:27:03 +0900

    [level 0] Title: 문자열 뒤집기, Time: 8.71 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,19 +1,20 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        
+        for(int i = 0; i < my_string.length(); i++){
+            answer = answer + my_string.charAt(my_string.length()-1-i);
+        }
+        
+        return answer;
+   }
+}
+
+// StringBuilder 를 사용한 코드
+//
 // class Solution {
 //     public String solution(String my_string) {
-//         String answer = "";
         
-//         return new answer(my_string).reverse().toString();
-        
-//         for(int i = 0; i < my_string.length(); i++){
-//             answer = answer + my_string.charAt(my_string.length()-1-i);
-//         }
-        
-//         return answer;
-//    }
+//         return new StringBuilder(my_string).reverse().toString();
+//     }
 // }
-
-class Solution {
-    public String solution(String myString) {
-        return new StringBuilder(myString).reverse().toString();
-    }
-}
