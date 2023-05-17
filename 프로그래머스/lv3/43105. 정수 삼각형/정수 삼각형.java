5df9647e3302ea92439a3c3af6c16784070573b3
ehdgus0221 <66156319+ehdgus0221@users.noreply.github.com>
2023-05-17 17:25:25 +0900

    [level 3] Title: 정수 삼각형, Time: 7.67 ms, Memory: 58.5 MB -BaekjoonHub

--- 프로그래머스/lv3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/lv3/43105. 정수 삼각형/정수 삼각형.java
@@ -10,12 +10,12 @@ class Solution {
             dp[i][0] = dp[i - 1][0] + triangle[i][0];
 
             // 중간에 있는 수들의 합
-                for (int j = 1; j <= i; j++) {
+                for (int j = 1; j < i; j++) {
                      dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - 1]) + triangle[i][j]; // 양쪽중 큰 값과 이전값의 합
                     }
                     // 맨 오른쪽줄의 합
-                    dp[i][i] = dp[i - 1][i - 1] + triangle[i][i];
-                }
+            dp[i][i] = dp[i - 1][i - 1] + triangle[i][i];        
+            }
 
                 for (int i = 0; i < triangle.length; i++) {
                     answer = Math.max(answer, dp[triangle.length - 1][i]);
