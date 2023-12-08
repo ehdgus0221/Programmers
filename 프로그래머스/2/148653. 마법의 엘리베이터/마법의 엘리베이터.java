617b1986a7e7b5de1250e44c0d8a59e0fc234b7b
ehdgus0221 <66156319+ehdgus0221@users.noreply.github.com>
2023-12-08 21:10:52 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.02 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(int storey) {
+        int num = storey;
+        int result = 0;
+        while(num > 0){
+            int temp = num % 10;
+            if (temp < 5){
+                result += temp;
+            } else if (temp == 5){
+                int tenCnt = (num % 100) / 10;
+                if (tenCnt >= 5){
+                    num += 10;
+                }
+                result += 5;
+            } else {
+                result += 10-temp;
+                num += 10;
+            }
+            num /= 10;
+        }
+        return result;
+    }
+}
