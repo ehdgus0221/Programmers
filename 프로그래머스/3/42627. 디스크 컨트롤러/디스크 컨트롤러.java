44fdfca3b32bbf26273ce85e4ee8ab60d4a7e37c
ehdgus0221 <66156319+ehdgus0221@users.noreply.github.com>
2023-12-16 12:04:12 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.70 ms, Memory: 71.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+class Solution {
+public static int solution(int[][] jobs) {
+
+    // 요청시간 기준 오름차순 정렬
+      Arrays.sort(jobs, new Comparator<int[]>() {
+          public int compare(int[] o1, int[] o2) {
+              if(o1[0] <= o2[0]){
+                  return -1;
+              }
+              return 1;
+          }
+      }); 
+      
+    // 소요시간 기준 오름차순 정렬
+      PriorityQueue<int[]> queue = new PriorityQueue<int[]>(new Comparator<int[]>() {
+          public int compare(int[] o1, int[] o2) {
+              if(o1[1] < o2[1]){
+                  return -1;
+              }
+              return 1;
+          }
+      });
+
+      int time = 0;
+      int index = 0;
+      float answer = 0;
+
+      while(true){
+          while(index < jobs.length && jobs[index][0] <= time){
+              queue.offer(jobs[index]);
+              index ++;
+          }
+          if(queue.size() == 0){
+              time = jobs[index][0];
+              continue;
+          }
+          int[] job = queue.poll();
+          time += job[1];
+          answer += time - job[0];
+          if(index == jobs.length && queue.size() == 0){
+              break;
+          }
+      }
+
+      answer /= jobs.length;
+      return (int)answer;
+    }
+}
